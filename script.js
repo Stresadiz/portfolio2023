ccc298bf62d9a5d2d7be7b04b9b6df3a583261dd
Sebastian Luna <eliaslunaslo@gmail.com>
2023-03-25 04:36:03 -0300

    Web complete

--- script.js
+++ script.js
@@ -26,3 +26,56 @@ span.addEventListener("mouseout", () => {
     emoji.style.width = "0em";
     emoji.style.transition = "0.5s";
 });
+
+
+let emailPlaceholder = document.querySelector(".wave--email");
+let textPlaceholder = document.querySelector(".wave--msg");
+const inputEmail = document.querySelector("#email");
+const inputText = document.querySelector("#text");
+
+let valueEmail = "email";
+let valueMessage = "message";
+
+for (let i = 0; i < valueEmail.length; i++) {
+    emailPlaceholder.innerHTML += `<span class="email--char">${valueEmail[i]}</span>`
+}
+
+for (let i = 0; i < valueMessage.length; i++) {
+    textPlaceholder.innerHTML += `<span class="text--char">${valueMessage[i]}</span>`
+}
+
+let letrasEmail = document.querySelectorAll(".email--char");
+let letrasMessage = document.querySelectorAll(".text--char");
+
+let leftEmail = 0;
+
+letrasEmail.forEach(letras => {
+    letras.style.left = leftEmail + 'px';
+    leftEmail += 16;
+});
+
+let leftText = 0;
+
+letrasMessage.forEach(letraMessage => {
+    letraMessage.style.left = leftText + 'px';
+    leftText += 16;
+});
+
+let transitionEmail = 0.2;
+let transitionMessage = 0.2;
+
+inputEmail.addEventListener("click", ()=> {
+    letrasEmail.forEach(letra => {
+        letra.style.top = "-1.5em";
+        letra.style.transition = transitionEmail + 's';
+        transitionEmail += 0.2;
+    });
+})
+
+inputText.addEventListener("click", ()=> {
+    letrasMessage.forEach(letra => {
+        letra.style.top = "-1.5em";
+        letra.style.transition = transitionMessage + 's';
+        transitionMessage += 0.2;
+    });
+})
